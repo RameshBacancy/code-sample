57ef2d08a05df09f9cae11634df82446646c8a29
ramesh <ramesh.prajapati@bacancytechnology>
2020-10-14 11:04:26 +0530

    sample code files

--- customer-jobs/customer-jobs.module.ts
+++ customer-jobs/customer-jobs.module.ts
@@ -8,7 +8,6 @@ import { NewJobComponent } from './new-job/new-job.component';
 import { JobDetailComponent } from './job-detail/job-detail.component';
 import { JobsBusinessProfileComponent } from './jobs-business-profile/jobs-business-profile.component';
 import { CustomerReviewComponent } from './customer-review/customer-review.component';
-// import { TenderAcceptReasonComponent } from './tender-accept-reason/tender-accept-reason.component';
 import { TenderRejectReasonComponent } from './tender-reject-reason/tender-reject-reason.component';
 import { JobsBusinessOrderSummaryProfileComponent } from './jobs-business-order-summary-profile/jobs-business-order-summary-profile.component';
 
@@ -19,7 +18,6 @@ import { JobsBusinessOrderSummaryProfileComponent } from './jobs-business-order-
     JobDetailComponent,
     CustomerReviewComponent,
     JobsBusinessProfileComponent,
-    // TenderAcceptReasonComponent,
     TenderRejectReasonComponent,
     JobsBusinessOrderSummaryProfileComponent
   ],
